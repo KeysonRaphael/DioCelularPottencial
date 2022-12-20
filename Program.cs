08d5b3b202a97e73dbedb6921968f43523db4cb3
Keyson Raphael <keyson.rapha@gmail.com>
2022-12-19 22:46:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DioCelularPottencial.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"123456",modelo:"Modelo 1",imei:"11111111111",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero:"123456",modelo:"Modelo 1",imei:"11111111111",memoria:64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
